9b6e22c56a2bad4ed3e883f8ecc2fdb60b439b3b
julieproductivity <113344890+julieproductivity@users.noreply.github.com>
2022-11-02 11:49:05 +0100

    passed test 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,14 @@
 const repeatString = function(string, num) {
-let output="";
+   let output="";
+   if (num<0) return output='ERROR';
 
-   while(num > 0) {
-    output += string;
-    num++;
-   }
-   return output;
+ while(num > 0) {
+         output += string;
+         num--;
+         
+      }
+      return output;
+
 }
 
 // Do not edit below this line
