3884647cb3942979201d11f15846355cc6b5ddf3
julieproductivity <113344890+julieproductivity@users.noreply.github.com>
2022-11-05 22:49:49 +0100

    reverse test pass

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+let splitInput = string.split('');
+let reverseInput = splitInput.reverse();
+let joinInput = reverseInput.join('');
+return joinInput;
 
 };
 
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,i) {
 
+    let removeArr = arr.slice(0,i);
+    let removedArrItem = arr(i);
+    let concatArray = removeArr.concat(removedArrItem);
+    return concatArray;
 };
 
 // Do not edit below this line
