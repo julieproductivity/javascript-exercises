8b787fcca8fe2151eab8c5b6a4a0a9a3522652b5
julieproductivity <113344890+julieproductivity@users.noreply.github.com>
2022-12-07 22:13:31 +0100

    done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,7 +1,26 @@
-const findTheOldest = function(obj) {
-// determine each people age and then rank them?
-//Js.date and then log age
-//js.date and then determine age and then rank
+const getAge = function(birth,death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    } return death-birth;
+}
+
+const findTheOldest = function(people) {
+/* let oldest = {};
+let oldestAge = 0;
+for (let i = 0; i < people.length; i++) {
+    let age = (people[i].yearOfDeath) - (people[i].yearOfBirth);
+    if (age >= oldestAge) {
+        oldestAge = age;
+        oldest = people[i];
+    }
+    
+} return oldest; */
+
+return people.reduce((oldest,currentPerson)=>{
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearofDeath);
+    const currentAge = getAge (currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+    return oldestAge < currentAge ? currentPerson : oldest;
+});
 
 };
 
